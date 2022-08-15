f2fea61685b5b275de9fa6678873ac8e550e6004
Artem Kormiltsov <akormilcov@rvision.pro>
2022-08-15 20:50:22 +0300

    add faker in RegistrationFormData

--- src/test/java/com/demoqa/data/RegistrationFormData.java
+++ src/test/java/com/demoqa/data/RegistrationFormData.java
@@ -1,19 +1,25 @@
 package com.demoqa.data;
 
 
+import com.github.javafaker.Faker;
+import org.apache.commons.lang3.RandomUtils;
+
 public class RegistrationFormData {
-    public String firstName = "Mark";
-    public String lastName = "Walker";
-    public String email = "markwalk@gmail.com";
-    public String mobilePhone = "8905555323";
+
+    Faker faker = new Faker();
+
+    public String firstName = faker.name().firstName();
+    public String lastName = faker.name().lastName();
+    public String email = faker.internet().emailAddress();
+    public String mobilePhone = faker.phoneNumber().subscriberNumber(10);
     public String subjects = "Economics";
     public String gender = "Male";
     public String fileName = "logo.png";
     public String filePath = fileName;
-    public String currentAddress = "Back Street";
-    public String year = "1990";
+    public String currentAddress = faker.address().fullAddress();
+    public String year = faker.number().numberBetween(1990, 2010) + "";
     public String month = "April";
-    public String day = "15";
+    public String day = faker.number().numberBetween(1, 28) + "";
     public String hobby = "Sports";
     public String state = "Uttar Pradesh";
     public String city = "Agra";
